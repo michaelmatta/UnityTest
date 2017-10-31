3b3ceea69334b140dc458351a6f1dd0aa43cdcdd
Michael Matta <mmmatta11@gmail.com>
2017-10-31 11:45:40 -0400

    testing debug log thing

---
+++ Unity Test GitHub/Assets/ScriptTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("cup of cheese");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
